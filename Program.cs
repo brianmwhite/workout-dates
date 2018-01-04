2b3cd5ce33d6d2302cb584b57384216d0437d0be
Brian White <brian@thirdember.com>
2018-01-04 09:55:39 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace workout_dates
+{
+    public static class Program
+    {
+        private static void Main(string[] args)
+        {
+            int year = DateTime.Now.Year;
+            if (args.Length > 0) { int.TryParse(args[0], out year); }
+
+            var startDate = new DateTime(year,1,1);
+            var endDate = new DateTime(year,12,31);
+
+            for (var i = startDate; i<=endDate; i = i.AddDays(1))
+            {
+                switch (i.DayOfWeek)
+                {
+                    case DayOfWeek.Monday:
+                    case DayOfWeek.Thursday:
+                        Console.WriteLine(i.ToString("MM/dd/yyyy"));
+                        break;
+                }
+            }
+       }
+    }
+}
